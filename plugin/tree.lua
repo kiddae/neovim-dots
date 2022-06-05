7a8baa3cf7ca7f88f00c7dd6751a57953a4530d3
Louise Nguyen <nguyenlelouise@gmail.com>
2022-06-05 18:53:10 +0200

    tree.nvim new configuration scheme

--- plugin/tree.lua
+++ plugin/tree.lua
@@ -1,29 +1,5 @@
-vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_respect_buf_cwd = 1
-vim.g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-        deleted = "",
-        ignored = "◌",
-        renamed = "➜",
-        staged = "✓",
-        unmerged = "",
-        unstaged = "✗",
-        untracked = "★",
-    },
-    folder = {
-        default = "",
-        empty = "",
-        empty_open = "",
-        open = "",
-        symlink = "",
-        symlink_open = "",
-        arrow_open = "",
-        arrow_closed = "",
-    },
-}
 require 'nvim-tree'.setup {
+    respect_buf_cwd = true,
     hijack_cursor = true,
     update_cwd = true,
     ignore_ft_on_setup = { "dashboard" },
@@ -39,7 +15,33 @@ require 'nvim-tree'.setup {
     renderer = {
         indent_markers = {
             enable = true
-        }
+        },
+        icons = {
+            glyphs = {
+                default = "",
+                symlink = "",
+                git = {
+                    deleted = "",
+                    ignored = "◌",
+                    renamed = "➜",
+                    staged = "✓",
+                    unmerged = "",
+                    unstaged = "✗",
+                    untracked = "★",
+                },
+                folder = {
+                    default = "",
+                    empty = "",
+                    empty_open = "",
+                    open = "",
+                    symlink = "",
+                    symlink_open = "",
+                    arrow_open = "",
+                    arrow_closed = "",
+                },
+            },
+        },
+        highlight_git = true,
     },
     diagnostics = {
         enable = false
