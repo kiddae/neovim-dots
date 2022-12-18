71d9e02012c65576903536c9a5a07c8ca1705467
Louise Nguyen <nguyenlelouise@gmail.com>
2022-12-18 15:56:20 +0100

    integration of dap with mason.nvim

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -56,7 +56,29 @@ require("mason-nvim-dap").setup_handlers()
 
 --- DAP-UI
 local dapui = require("dapui")
-dapui.setup()
+dapui.setup({
+    layouts = {
+        {
+            elements = {
+                -- Elements can be strings or table with id and size keys.
+                { id = "scopes", size = 0.25 },
+                "breakpoints",
+                "stacks",
+                "watches",
+            },
+            size = 40, -- 40 columns
+            position = "left",
+        },
+        {
+            elements = {
+                -- "repl",
+                "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+        },
+    },
+})
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
 end
